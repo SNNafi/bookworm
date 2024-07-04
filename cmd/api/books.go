cfa1409cf1048de6a465ece1f17cb37e301566a0
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2024-07-04 06:09:22 +0600

    Implement book enconding/deconding & validations

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"bookworm.snnafi.dev/internal/data"
+	"bookworm.snnafi.dev/internal/validator"
 	"fmt"
 	"net/http"
 	"time"
 )
 
 func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Create a new book")
+
+	var input struct {
+		Name       string          `json:"name"`
+		Author     string          `json:"author"`
+		Publisher  string          `json:"publisher"`
+		Image      string          `json:"image"`
+		CoverImage string          `json:"cover_image"`
+		Type       []data.BookType `json:"type"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	book := &data.Book{
+		Name:       input.Name,
+		Author:     input.Author,
+		Publisher:  input.Publisher,
+		Image:      input.Image,
+		CoverImage: input.CoverImage,
+		Type:       input.Type,
+	}
+
+	v := validator.New()
+
+	if book.ValidateBook(v); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showBookHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +54,7 @@ func (app *application) showBookHandler(w http.ResponseWriter, r *http.Request) 
 	book := data.Book{
 		ID:         id,
 		Name:       "সীরাহ ১ম খণ্ড",
-		Writer:     "রেইনড্রপস মিডিয়া",
+		Author:     "রেইনড্রপস মিডিয়া",
 		Publisher:  "রেইনড্রপস মিডিয়া",
 		Image:      "seerahlastpart.webp",
 		CoverImage: "seerah_cover.webp",
--- internal/data/book.go
+++ internal/data/book.go
@@ -1,8 +1,12 @@
 package data
 
 import (
+	"bookworm.snnafi.dev/internal/validator"
+	"errors"
 	"time"
 )
+
+var ErrInvalidBookTypeFormat = errors.New("invalid book type format")
 
 type BookType int
 
@@ -16,6 +20,18 @@ func (t BookType) MarshalJSON() ([]byte, error) {
 	return []byte(`""`), nil
 }
 
+func (t *BookType) UnmarshalJSON(b []byte) error {
+	switch string(b) {
+	case `"Islamic"`:
+		*t = 1
+	case `"Comparative Religion"`:
+		*t = 2
+	default:
+		return ErrInvalidBookTypeFormat
+	}
+	return nil
+}
+
 const (
 	Islamic BookType = iota + 1
 	ComparativeReligion
@@ -24,10 +40,21 @@ const (
 type Book struct {
 	ID         int64      `json:"id"`
 	Name       string     `json:"name"`
-	Writer     string     `json:"writer"`
+	Author     string     `json:"author"`
 	Publisher  string     `json:"publisher"`
 	Image      string     `json:"image"`
 	CoverImage string     `json:"cover_image,omitempty"`
 	Type       []BookType `json:"type,omitempty"`
 	CreatedAt  time.Time  `json:"-"`
+}
+
+func (book *Book) ValidateBook(v *validator.Validator) {
+	v.Check(book.Name != "", "name", "must be provided")
+	v.Check(book.Author != "", "author", "must be provided")
+	v.Check(book.Publisher != "", "author", "must be provided")
+	v.Check(book.Image != "", "image", "must be provided")
+	v.Check(book.Type != nil, "type", "must be provided")
+	v.Check(len(book.Type) >= 1, "type", "must contain at least 1 type")
+	v.Check(len(book.Type) <= 3, "type", "must not contain more than 3 types")
+	v.Check(validator.Unique(book.Type), "type", "must not contain duplicate values")
 }
